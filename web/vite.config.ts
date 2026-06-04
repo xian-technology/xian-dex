d6d3e694d4bcfdc69d5a88816e1e4a98a1b0530c
Endogen <Endogen@users.noreply.github.com>
2026-06-05 00:41:41 +0200

    Enable WASM support in Vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [wasm(), react()],
   resolve: { dedupe: ["react", "react-dom"] },
   server: { port: 5173 }
 });
